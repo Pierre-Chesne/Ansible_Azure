6f4e23ca12b0f642ea7636810179d93ceb2fed6e
Pierre-Chesne <pierrc@hotmail.com>
2020-12-15 17:35:12 +0100

    v.0.5

--- Vm-Ansible/by_bash/Install_Ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'alias python=python3' >> /home/pierrc/.bashrc
-echo 'StrictHostKeyChecking no' >> /home/pierrc/.ssh/config
-apt update && apt upgrade -y
-apt install python3-pip -y
-pip3 install ansible
-su -l pierrc -c "git clone git@github.com:Pierre-Chesne/Ansible_Azure.git /home/pierrc/Ansible_Azure"
